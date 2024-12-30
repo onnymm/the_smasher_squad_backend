3f4738b16c29ec11d0d7bae3ce0f3ad4c00f12ed
unknown <phr2807@gmail.com>
2024-12-29 23:24:42 -0700

    Corrección en el dominio frontend autorizado

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://thesmashersquad.vercel.app/",
+    "https://thesmashersquad.vercel.app",
 ]
 
 # Agregar el middleware de CORS
